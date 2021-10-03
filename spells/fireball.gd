2f8b808d5399eb51bbadfb8a6d4ddb7853149ab5
Mike Orlov <m.orlov@technokert.ru>
2021-10-03 17:40:02 +0300

    fix spells

--- spells/fireball.gd
+++ spells/fireball.gd
@@ -11,6 +11,14 @@ func _ready():
 	pass # Replace with function body.
 
 
+func cast(target_position: Vector2):
+	print('cast fireball: ', target_position)
+	var target_cell = Matrix.get_cell(target_position)
+	var target_unit = target_cell.unit
+	if target_unit != null:
+		Matrix.interact_with_unit(self, target_unit, "take_damage")
+		
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass

e45ff4804b2c95e4e90ceda0f2bd9f00364526bf
Ivan <i.vasiliev@technokert.ru>
2021-10-05 00:51:25 +0300

    vapire move

--- units/vampire.gd
+++ units/vampire.gd
@@ -7,11 +7,14 @@ var hint = ("Unit: vampire \nRace: undead \nMove: 'madly', in any direction"+
 "\nProperties: every 3 turns become weak; if weak - freeze random "+
 "non-undead neighbour unit and returns to normal")
 
+var is_week = false
+var turn_counter = 0
+var max_turn_counter = 3
 
 func _ready():
 	self._race =  "undead"
 	self._tier =  3
-	change_status(self ,"lightning_shield")
+#	change_status(self ,"lightning_shield")
 func get_race():
 	return "undead"
 
@@ -19,12 +22,21 @@ func get_tier():
 	return 3
 
 func _act():
-	var coordinates = Matrix.get_unit_coordinates(self)
-	var neighbors = Matrix.get_neighbors(coordinates)
-	if neighbors.right_neighbor:
-		if neighbors.right_neighbor.get_race() != self._race:
-			if neighbors.right_neighbor.get_status() != "frozen":
-				neighbors.right_neighbor.change_status(self, "frozen")
+#	if turn_counte == max_turn_counter:
+		
+		var coordinates = Matrix.get_unit_coordinates(self)
+		var neighbors_pos = filter_positions(coordinates)
+		var not_undead_neighbors = []
+		for neighbor_pos in neighbors_pos:
+			var cell = Matrix.get_cell(neighbor_pos)
+			if cell.unit:
+				if cell.unit.get_race() != self._race:
+					not_undead_neighbors.append(cell.unit)
+					
+		var neighbor_pos = neighbors_pos[randi() % neighbors_pos.size()]
+		var target =  not_undead_neighbors[randi() % not_undead_neighbors.size()]
+		if target:
+			target.change_status(self, "frozen")
 
-	wander_move()
+		wander_move()
 	

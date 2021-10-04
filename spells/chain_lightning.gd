f5384db90adb03833009394c31ab2babc3c3a6da
Mike Orlov <m.orlov@technokert.ru>
2021-10-04 22:24:24 +0300

    spells added

---
+++ spells/chain_lightning.gd
@@ -0,0 +1,5 @@
+extends Spell
+
+func cast(target_position: Vector2):
+	print('cast chain_lightning: ', target_position)
+	
---
+++ spells/frost_shard.gd
@@ -0,0 +1,5 @@
+extends Spell
+
+func cast(target_position: Vector2):
+	print('cast frost_shard: ', target_position)
+	
